698565412eb37b4e8eed66f9c1c7b33557bf3ddb
montanizstills <montanizstills@gmail.com>
2025-05-13 04:26:38 -0400

    minimally functionally kali image post gpg key expiry

--- docker/update-kali-docker-image-gpg-key.sh
+++ docker/update-kali-docker-image-gpg-key.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+# This script is used to update the Kali Linux GPG keyring for Docker images.
+
 set -x
-cp /media/sf_vbox/kali-archive-keyring.gpg /usr/share/keyrings/kali-archive-keyring.gpg
 expected_sha1="603374c107a90a69d983dbcb4d31e0d6eedfc325"
-sha1sum /usr/share/keyrings/kali-archive-keyring.gpg | grep -q "$expected_sha1" || {
-    echo "The SHA1 checksum does not match. Please check the file."
-    exit 1
-}
+key_ring_path="/media/sf_vbox/kali-archive-keyring.gpg"
+
+if ! [ -f $key_ring_path ]; then
+  echo "File /media/sf_vbox/kali-archive-keyring.gpg does not exist."
+  echo "Please download it from https://www.kali.org/blog/new-kali-archive-signing-key/ and upload it to the image docker 'run -v /media/sf_vbox:/media/sf_vbox -it kali-vbox'"
+  keyring_path="kali-archive-keyring.gpg"
+fi
+
+# cp "$keyring_path" /usr/share/keyrings/kali-archive-keyring.gpg
+cp /media/sf_vbox/kali-archive-keyring.gpg /usr/share/keyrings/kali-archive-keyring.gpg
+
+actual_sha1=$(sha1sum $key_ring_path | cut -d' ' -f1)
+
+if [ "$actual_sha1" != "$expected_sha1" ]; then
+  echo "SHA1 checksum does not match, please check https://www.kali.org/blog/new-kali-archive-signing-key/"
+  exit 1
+fi
+
+echo "Kali Linux GPG keyring updated successfully."
